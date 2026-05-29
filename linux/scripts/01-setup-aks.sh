820aa2bf45d66440e1d3c01b94d82f1ee5eb0a9a
AjayPulapa <109037217+AjayPulapa@users.noreply.github.com>
2026-05-30 00:54:15 +0530

    Update 01-setup-aks.sh

--- linux/scripts/01-setup-aks.sh
+++ linux/scripts/01-setup-aks.sh
@@ -4,17 +4,34 @@ RESOURCE_GROUP="rg-nginx-dev"
 LOCATION="centralindia"
 AKS_CLUSTER="aks-nginx-dev"
 
-az group create --name $RESOURCE_GROUP --location $LOCATION
+echo "======================================="
+echo "Creating Resource Group"
+echo "======================================="
+
+az group create \
+  --name $RESOURCE_GROUP \
+  --location $LOCATION
+
+echo "======================================="
+echo "Creating AKS Cluster"
+echo "======================================="
 
 az aks create \
   --resource-group $RESOURCE_GROUP \
   --name $AKS_CLUSTER \
   --node-count 1 \
-  --enable-addons monitoring \
   --generate-ssh-keys
+
+echo "======================================="
+echo "Connecting kubectl"
+echo "======================================="
 
 az aks get-credentials \
   --resource-group $RESOURCE_GROUP \
   --name $AKS_CLUSTER
+
+echo "======================================="
+echo "Kubernetes Nodes"
+echo "======================================="
 
 kubectl get nodes
