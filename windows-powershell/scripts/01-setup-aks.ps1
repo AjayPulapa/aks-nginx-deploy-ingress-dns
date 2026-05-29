1791e24653653701094a5614592968eb3cf36af0
AjayPulapa <109037217+AjayPulapa@users.noreply.github.com>
2026-05-30 00:54:28 +0530

    Update 01-setup-aks.ps1

--- windows-powershell/scripts/01-setup-aks.ps1
+++ windows-powershell/scripts/01-setup-aks.ps1
@@ -2,16 +2,27 @@ $RESOURCE_GROUP="rg-nginx-dev"
 $LOCATION="centralindia"
 $AKS_CLUSTER="aks-nginx-dev"
 
+Write-Host "======================================="
+Write-Host "Creating Resource Group"
+Write-Host "======================================="
+
 az group create `
   --name $RESOURCE_GROUP `
   --location $LOCATION
+
+Write-Host "======================================="
+Write-Host "Creating AKS Cluster"
+Write-Host "======================================="
 
 az aks create `
   --resource-group $RESOURCE_GROUP `
   --name $AKS_CLUSTER `
   --node-count 1 `
-  --enable-addons monitoring `
   --generate-ssh-keys
+
+Write-Host "======================================="
+Write-Host "Connecting kubectl"
+Write-Host "======================================="
 
 az aks get-credentials `
   --resource-group $RESOURCE_GROUP `
